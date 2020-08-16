58400e6acf2b4415b64213b44d97eb40d7fa72a1
Peter Miller <68311011+peteremiller@users.noreply.github.com>
2020-08-16 18:23:14 -0500

    Delete temperature.py

--- Election_Analysis/Practice_py_files/temperature.py
+++
@@ -1,8 +0,0 @@
-#counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
-#for county, voters in counties_dict.items():
- #   print(f"{county} county has {voters:,.0f} registered voters.")
-
-voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
-{"county":"Denver", "registered_voters": 463353},
-{"county":"Jefferson", "registered_voters": 432438}] 
-print(f'{county} county has {voters:,.0f} registered voters.')
